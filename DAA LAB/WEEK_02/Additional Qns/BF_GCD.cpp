1d95efac640e3790d60b293650d470ede585c84f
AxRut <85568731+ashrut1011@users.noreply.github.com>
2023-02-26 10:58:35 +0530

    Update BF_GCD.cpp

--- DAA LAB/WEEK_02/Additional Qns/BF_GCD.cpp
+++ DAA LAB/WEEK_02/Additional Qns/BF_GCD.cpp
@@ -1,3 +1,5 @@
+// Write a program to find GCD using BRUTEFORCE method
+
 #include <iostream>
 using namespace std;
 
@@ -42,5 +44,5 @@ int main() {
         
         cout << "GCD of " << a << " and " << b << " is " << result << endl;
     }
-    return 0;
+    return 69;
 }
