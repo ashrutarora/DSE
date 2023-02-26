7af87ce4a5534c58b2da0b0ffaa99bb78cbba52c
AxRut <85568731+ashrut1011@users.noreply.github.com>
2023-02-26 10:57:22 +0530

    Update and rename MiddleSkl.cpp to 2.3_MiddleSkl.cpp

--- DAA LAB/WEEK_02/2.3_MiddleSkl.cpp
+++ DAA LAB/WEEK_02/2.3_MiddleSkl.cpp
@@ -1,27 +1,36 @@
+// Write a program to find GCD using middle school method and analyze its time efficiency
+
 #include <iostream>
 
 using namespace std;
 
-// function to calculate prime factors of a given number
+// Function to calculate prime factors of a given number
 int primeFac(int n, int factors[]) {
+    
     int count = 0;
+    
     while (n % 2 == 0) {
+        
         factors[count++] = 2;
         n /= 2;
     }
+    
     for (int i = 3; i * i <= n; i += 2) {
+        
         while (n % i == 0) {
+            
             factors[count++] = i;
             n /= i;
         }
     }
+    
     if (n > 2) {
+        
         factors[count++] = n;
     }
     return count;
 }
 
-// function to calculate the gcd of two numbers using the middle school method
 int gcd(int m, int n) {
 
     int factors_m[100], factors_n[100];
@@ -33,28 +42,34 @@ int gcd(int m, int n) {
     int count_common = 0;
 
     for (int i = 0; i < count_m; i++) {
+        
         for (int j = 0; j < count_n; j++) {
+            
             if (factors_m[i] == factors_n[j]) {
+                
                 common_factors[count_common++] = factors_m[i];
                 factors_n[j] = -1;
+                
                 break;
             }
         }
     }
+    
     int result = 1;
     for (int i = 0; i < count_common; i++) {
+        
         result *= common_factors[i];
     }
     return result;
 }
 
 int main() {
-    int a, b;
-    int c;
-    int exit_code = 49;
+    int a, b, c;
+   
     cout << "Enter any two numbers : ";
     cin >> a >> b;
-    c = gcd(a, b);
-    cout << "GCD of " << a << " and " << b << " using MIDDLE SCHOOL method " << " = " << c << endl;
-    return 0;
+    
+    cout << "GCD of " << a << " and " << b << " using MIDDLE SCHOOL method " << " = " << gcd(a, b); << endl;
+    
+    return 69;
 }
