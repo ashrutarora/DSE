e98f283b66cf222a51bf5c6f494d8d7ce039e217
Ashrut Alok Arora <85568731+ashrut1011@users.noreply.github.com>
2023-03-06 09:08:19 +0530

    Update 3.1_SelectionSort.cpp

--- DAA LAB/WEEK_03/3.1_SelectionSort.cpp
+++ DAA LAB/WEEK_03/3.1_SelectionSort.cpp
@@ -49,7 +49,7 @@ int main(){
 
     SelecSort(arr, size);
 
-    cout << "Sorted Array using INSERTION SORT" << endl;
+    cout << "Sorted Array using SELECTION SORT" << endl;
 
     for(int i =0; i < size; i++){
 
