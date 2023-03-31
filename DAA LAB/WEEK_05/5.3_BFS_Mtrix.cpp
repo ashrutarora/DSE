1266b18915777f46a6f130bfd515c3b6c6204928
Ashrut Alok Arora <85568731+ashrut1011@users.noreply.github.com>
2023-04-01 00:34:21 +0530

    Update 5.3_BFS_Mtrix.cpp

--- DAA LAB/WEEK_05/5.3_BFS_Mtrix.cpp
+++ DAA LAB/WEEK_05/5.3_BFS_Mtrix.cpp
@@ -3,94 +3,124 @@ using namespace std;
 
 const int MAX_VERTICES = 100;
 
-// Structure to represent a queue
-struct Queue {
+// Class to represent a queue
+class Queue
+{
+private:
     int front, rear;
     int arr[MAX_VERTICES];
+
+public:
+    // Constructor to initialize the queue
+    Queue()
+    {
+        front = rear = -1;
+    }
+
+    // Function to check if the queue is empty
+    bool isEmpty()
+    {
+        return front == -1;
+    }
+
+    // Function to check if the queue is full
+    bool isFull()
+    {
+        return (rear + 1) % MAX_VERTICES == front;
+    }
+
+    // Function to add an element to the rear of the queue
+    void enqueue(int x)
+    {
+        if (isFull())
+        {
+            cout << "Queue overflow!" << endl;
+            return;
+        }
+        if (isEmpty())
+        {
+            front = rear = 0;
+        }
+        else
+        {
+            rear = (rear + 1) % MAX_VERTICES;
+        }
+        arr[rear] = x;
+    }
+
+    // Function to remove an element from the front of the queue
+    int dequeue()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue underflow!" << endl;
+            return -1;
+        }
+        int x = arr[front];
+        if (front == rear)
+        {
+            front = rear = -1;
+        }
+        else
+        {
+            front = (front + 1) % MAX_VERTICES;
+        }
+        return x;
+    }
 };
 
-// Function to initialize the queue
-void initQueue(Queue* q) {
-    q->front = q->rear = -1;
-}
-
-// Function to check if the queue is empty
-bool isEmpty(Queue* q) {
-    return q->front == -1;
-}
-
-// Function to check if the queue is full
-bool isFull(Queue* q) {
-    return (q->rear + 1) % MAX_VERTICES == q->front;
-}
-
-// Function to add an element to the rear of the queue
-void enqueue(Queue* q, int x) {
-    if (isFull(q)) {
-        cout << "Queue overflow!" << endl;
-        return;
-    }
-    if (isEmpty(q)) {
-        q->front = q->rear = 0;
-    }
-    else {
-        q->rear = (q->rear + 1) % MAX_VERTICES;
-    }
-    q->arr[q->rear] = x;
-}
-
-// Function to remove an element from the front of the queue
-int dequeue(Queue* q) {
-    if (isEmpty(q)) {
-        cout << "Queue underflow!" << endl;
-        return -1;
-    }
-    int x = q->arr[q->front];
-    if (q->front == q->rear) {
-        q->front = q->rear = -1;
-    }
-    else {
-        q->front = (q->front + 1) % MAX_VERTICES;
-    }
-    return x;
-}
-
 // Function to perform BFS traversal of the graph
-void BFS(int adjMatrix[][MAX_VERTICES], int numVertices, int startVertex) {
-    bool visited[MAX_VERTICES] = { false };
+void BFS(int adjMatrix[][MAX_VERTICES], int numVertices, int startVertex)
+{
+    bool visited[MAX_VERTICES] = {false};
+    int poppedOrder[MAX_VERTICES], numPopped = 0;
     Queue q;
-    initQueue(&q);
-    enqueue(&q, startVertex);
+    q.enqueue(startVertex);
 
-    while (!isEmpty(&q)) {
-        int currVertex = dequeue(&q);
+    while (!q.isEmpty())
+    {
+        int currVertex = q.dequeue();
+        poppedOrder[numPopped++] = currVertex;
 
-        if (!visited[currVertex]) {
+        if (!visited[currVertex])
+        {
             visited[currVertex] = true;
             cout << currVertex << " ";
 
             // Visit all adjacent vertices of the current vertex
-            for (int i = 0; i < numVertices; i++) {
-                if (adjMatrix[currVertex][i] == 1 && !visited[i]) {
-                    enqueue(&q, i);
+            for (int i = 0; i < numVertices; i++)
+            {
+                if (adjMatrix[currVertex][i] == 1 && !visited[i])
+                {
+                    q.enqueue(i);
                 }
             }
         }
     }
+
+    cout << "\nPopped order: ";
+    for (int i = 0; i < numPopped; i++)
+    {
+        cout << poppedOrder[i] << " ";
+    }
 }
 
-int main() {
+
+
+int main()
+{
     int numVertices, numEdges;
-    
+
     cout << "Enter the number of vertices and edges: ";
     cin >> numVertices >> numEdges;
 
     // Create adjacency matrix for the graph
-    int adjMatrix[MAX_VERTICES][MAX_VERTICES] = { 0 };
-    
+    int adjMatrix[MAX_VERTICES][MAX_VERTICES] = {0};
+
     cout << "Enter the edges: " << endl;
-    
-    for (int i = 0; i < numEdges; i++) {
+
+    for (int i = 0; i < numEdges; i++)
+    {
         int u, v;
         cin >> u >> v;
         adjMatrix[u][v] = adjMatrix[v][u] = 1;
@@ -103,4 +133,3 @@ int main() {
 
     return 69;
 }
-
