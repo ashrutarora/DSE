94e0328c694b95002258cb9c6cb8590a547c49b4
Ashrut Alok Arora <85568731+ashrut1011@users.noreply.github.com>
2023-04-01 00:28:07 +0530

    Update 5.2_DFS_Mtrix.cpp

--- DAA LAB/WEEK_05/5.2_DFS_Mtrix.cpp
+++ DAA LAB/WEEK_05/5.2_DFS_Mtrix.cpp
@@ -3,72 +3,99 @@ using namespace std;
 
 const int MAX_VERTICES = 100;
 
-// Structure to represent a stack
-struct Stack {
+// Class to represent a stack
+class Stack
+{
+private:
     int top;
     int arr[MAX_VERTICES];
+
+public:
+    // Constructor to initialize the stack
+    Stack()
+    {
+        top = -1;
+    }
+
+    // Getter function to get the top element of the stack
+    int getTop()
+    {
+        return top;
+    }
+
+    // Function to check if the stack is empty
+    bool isEmpty()
+    {
+        return top == -1;
+    }
+
+    // Function to push an element onto the stack
+    void push(int x)
+    {
+        arr[++top] = x;
+    }
+
+    // Function to pop an element from the stack
+    int pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack underflow!" << endl;
+            return -1;
+        }
+        return arr[top--];
+    }
 };
 
-// Function to initialize the stack
-void initStack(Stack* s) {
-    s->top = -1;
-}
-
-// Function to check if the stack is empty
-bool isEmpty(Stack* s) {
-    return s->top == -1;
-}
-
-// Function to push an element onto the stack
-void push(Stack* s, int x) {
-    s->arr[++s->top] = x;
-}
-
-// Function to pop an element from the stack
-int pop(Stack* s) {
-    if (isEmpty(s)) {
-        cout << "Stack underflow!" << endl;
-        return -1;
-    }
-    return s->arr[s->top--];
-}
-
 // Function to perform DFS traversal of the graph
-void DFS(int adjMatrix[][MAX_VERTICES], int numVertices, int startVertex) {
-    bool visited[MAX_VERTICES] = { false };
-    Stack stk;
-    initStack(&stk);
-    push(&stk, startVertex);
+void DFS(int adjMatrix[][MAX_VERTICES], int numVertices, int startVertex)
+{
+    bool visited[MAX_VERTICES] = {false};
+    Stack stk, poppedStk;
+    stk.push(startVertex);
 
-    while (!isEmpty(&stk)) {
-        int currVertex = pop(&stk);
+    while (!stk.isEmpty())
+    {
+        int currVertex = stk.pop();
+        poppedStk.push(currVertex);
 
-        if (!visited[currVertex]) {
+        if (!visited[currVertex])
+        {
             visited[currVertex] = true;
             cout << currVertex << " ";
 
             // Visit all adjacent vertices of the current vertex
-            for (int i = 0; i < numVertices; i++) {
-                if (adjMatrix[currVertex][i] == 1 && !visited[i]) {
-                    push(&stk, i);
+            for (int i = 0; i < numVertices; i++)
+            {
+                if (adjMatrix[currVertex][i] == 1 && !visited[i])
+                {
+                    stk.push(i);
                 }
             }
         }
     }
+
+    cout << endl << "Popping order: ";
+    while (!poppedStk.isEmpty())
+    {
+        cout << poppedStk.pop() << " ";
+    }
 }
 
-int main() {
+int main()
+{
     int numVertices, numEdges;
-    
+
     cout << "Enter the number of vertices and edges: ";
     cin >> numVertices >> numEdges;
 
     // Create adjacency matrix for the graph
-    int adjMatrix[MAX_VERTICES][MAX_VERTICES] = { 0 };
-    
+    int adjMatrix[MAX_VERTICES][MAX_VERTICES] = {0};
+
     cout << "Enter the edges: " << endl;
-    
-    for (int i = 0; i < numEdges; i++) {
+
+    for (int i = 0; i < numEdges; i++)
+    {
         int u, v;
         cin >> u >> v;
         adjMatrix[u][v] = adjMatrix[v][u] = 1;
@@ -77,8 +104,6 @@ int main() {
     // Perform DFS traversal starting from vertex 0
     cout << "DFS traversal of the graph: ";
     DFS(adjMatrix, numVertices, 0);
-    cout << endl;
 
     return 69;
 }
-
